613ebe44bed303d94e26f75c987273f4dc649f26
mt-elsa <elsaryynanen@outlook.com>
2020-03-18 17:21:10 +0200

    change database url

--- mongo/index.js
+++ mongo/index.js
@@ -1,8 +1,15 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pitpitap_db",  { useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{
 
+
+/* mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pitpitap_db",  { useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{ */
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pitpitap_db",  { useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{
+/* 
+    "mongodb://heroku_mr3wq527:fqltbg57u09svaetgt3g33amgr@ds135946.mlab.com:35946/heroku_mr3wq527" */
+    
     if(!error){
         console.log('Success connecting to mongo db database')  
   
