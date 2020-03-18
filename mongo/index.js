81933a8fdf14112e25c513b514c36b3bb7f54d69
mt-elsa <elsaryynanen@outlook.com>
2020-03-18 14:07:35 +0200

    first commit

--- mongo/index.js
+++ mongo/index.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/pitpitap_db",  { useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pitpitap_db",  { useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{
 
     if(!error){
         console.log('Success connecting to mongo db database')  
